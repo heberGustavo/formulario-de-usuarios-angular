bedc35d360121e30ecc4b6166f7f6a3afe36d4cf
heberGustavo <heber.gbarbosa@hotmail.com>
2026-05-06 08:08:00 -0300

    Creation of the principal grid and UserList Component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from "@angular/core";
 
 import { PipesModule } from "../pipes/pipes.module";
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        UsersListComponent
+    ],
     imports: [
         PipesModule,
         AngularMaterialModule,
     ],
-    exports: [],
+    exports: [
+        UsersListComponent,
+    ],
 })
 export class ComponentsModule {
 
