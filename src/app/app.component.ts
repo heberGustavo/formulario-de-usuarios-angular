e798cd7f250bb6d0c06b21464709f839f2f9e72f
heberGustavo <heber.gbarbosa@hotmail.com>
2026-05-06 06:15:03 -0300

    Creation of the Enums

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './services/countries.service';
 import { StatesService } from './services/states.service';
 import { CitiesService } from './services/cities.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,13 @@ export class AppComponent implements OnInit {
     private readonly _countriesService: CountriesService,
     private readonly _statesService: StatesService,
     private readonly _citiesService: CitiesService,
+    private readonly _usersService: UsersService
   ) { }
 
   ngOnInit() {
     //this._countriesService.getCountries().subscribe((data) => { console.log("Countries: ", data) });
     //this._statesService.getStates('Brazil').subscribe((data) => console.log("States: ", data));
-    this._citiesService.getCitiesInCountryAndState('Brazil', 'Acre').subscribe((data) => console.log("Cities: ", data));
+    //this._citiesService.getCitiesInCountryAndState('Brazil', 'Acre').subscribe((data) => console.log("Cities: ", data));
+    this._usersService.getUsers().subscribe((data) => console.log("Users: ", data));
   }
 }
